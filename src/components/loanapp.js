cb3c3b1e8ed58787f2b3a5910bd99a07bc4390e6
Chris DeSilva <chris.a.desilva@gmail.com>
2019-10-25 15:39:51 -0500

    Config gtag events

--- src/components/loanapp.js
+++ src/components/loanapp.js
@@ -166,13 +166,13 @@ const LoanApp = React.forwardRef((props, ref) => {
         window.open(loanUrl, "_blank", "noopener noreferrer")
     };
 
-    const trackGoogleAnalyticsEvent = () => {
-            ReactGA.event({
-                category: 'Apply Now Button | DevMountain',
-                action: 'click',
-                label: 'submitted loan application'
-            })
-    }
+    // const trackGoogleAnalyticsEvent = () => {
+    //         ReactGA.event({
+    //             category: 'Apply Now Button | DevMountain',
+    //             action: 'click',
+    //             label: 'submitted loan application'
+    //         })
+    // }
 
     const trackFacebookPixel = () => {
         ReactPixel.track('InitiateCheckout', {
@@ -239,7 +239,7 @@ const LoanApp = React.forwardRef((props, ref) => {
         .then(response => console.log('success', response))
         .catch(error => console.log('error: ', error))
         
-        trackGoogleAnalyticsEvent()
+        // trackGoogleAnalyticsEvent()
         trackFacebookPixel()
         redirectLoanApp()
         isSubmitted(true)
